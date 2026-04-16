aa92ebe8e15d8ef852585efcdae34a49cb0b4543
Kemogaber <halabykareem@gmail.com>
2026-04-17 00:48:06 +0200

    Portfolio Website Initiated

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <section id='about' className="py-24 px-4 relative"></section>
+  )
+}
+
+export default AboutSection
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+      {/* Theme toggle */}
+        <ThemeToggle/>
+      {/* Background effects */}
+      <StarBackground/>
+      {/* Nav Bar */}
+      <Navbar/>
+      {/* Main Content */}
+      <main>
+        <HeroSection/>
+        <AboutSection/>
+      </main>
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
