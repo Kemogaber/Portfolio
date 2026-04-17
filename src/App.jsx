540987c4a788a0517fe515b06a041c453f5c1d44
Kemogaber <halabykareem@gmail.com>
2026-04-17 04:45:29 +0200

    changed basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
     <Routes>
       <Route index element={<Home/>}/>
       <Route path='*' element={<NotFound/>}/>
